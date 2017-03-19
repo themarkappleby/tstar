378c3d2fb1bdea355e18a8505e9e1f4e54651928
themarkappleby <themarkappleby@gmail.com>
2017-03-19 11:59:11 -0400

    mobile improvements

--- src/upload.js
+++ src/upload.js
@@ -1,22 +1,31 @@
+var fs = require('fs')
 var AWS = require('aws-sdk')
 var s3 = new AWS.S3({
   endpoint: 's3.ca-central-1.amazonaws.com',
   signatureVersion: 'v4',
   region: 'ca-central-1'
 })
-var fs = require('fs')
+
+var files = [
+  './dist/index.html',
+  './dist/scripts.js',
+  './dist/styles.css',
+  './dist/icon.png'
+]
+
+var types = {
+  html: 'text/html',
+  js: 'text/javascript',
+  css: 'text/css',
+  png: 'image/png'
+}
 
 function init () {
-  var files = [
-    './dist/index.html',
-    './dist/scripts.js',
-    './dist/styles.css'
-  ]
-
+  console.log('Uploading to: ' + s3.endpoint)
   files.forEach(function (file) {
     var name = file.split('/')
     name = name[name.length - 1]
-    fs.readFile(file, function (err, data) {
+    fs.readFile(file, 'utf-8', function (err, data) {
       if (err) throw err
       upload(name, data)
     })
@@ -24,15 +33,17 @@ function init () {
 }
 
 function upload (name, body) {
+  var ext = name.split('.')[1]
   var param = {
-    Bucket: 'tstar-app',
+    Bucket: 'tstar.com',
     Key: name,
-    Body: body
+    Body: body,
+    ContentDisposition: 'inline',
+    ContentType: types[ext]
   }
   s3.upload(param, function (err, data) {
     if (err) console.log(err, err.stack)
-    console.log('actually done!')
-    // context.done()
+    console.log('Uploaded: ' + name)
   })
 }
 
